5a7049088d487308debe6008c8aa5a2cef02db33
Alive <nicolasfr002@gmail.com>
2024-05-29 17:39:55 +0200

    refactor(project): moved main in own class
    - for better namings
    
    Took 2 minutes

---
+++ src/main/java/de/alive/regexbuilder/Main.java
@@ -0,0 +1,7 @@
+package main.java.de.alive.regexbuilder;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+    }
+}
--- src/main/java/de/alive/regexbuilder/RegexEnumerator.java
+++ src/main/java/de/alive/regexbuilder/RegexEnumerator.java
@@ -1,7 +1,5 @@
 package main.java.de.alive.regexbuilder;
 
 public class RegexEnumerator {
-    public static void main(String[] args) {
-        System.out.println("Hello world!");
-    }
+
 }
