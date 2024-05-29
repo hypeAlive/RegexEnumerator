4811e84bb8bb21edeeca9bd4d725e78cf710a648
Alive <nicolasfr002@gmail.com>
2024-05-29 17:52:47 +0200

    feat(enumerator): implemented enumerator code
    - use a RegexAlphabet and RegexOperationString instance
    - enumerate with limit
    - performance with queue and HashSet
    
    Took 12 minutes

--- src/main/java/de/alive/regexbuilder/RegexEnumerator.java
+++ src/main/java/de/alive/regexbuilder/RegexEnumerator.java
@@ -1,5 +1,53 @@
 package main.java.de.alive.regexbuilder;
 
+import main.java.de.alive.regexbuilder.utils.RegexAlphabet;
+import main.java.de.alive.regexbuilder.utils.RegexOperationString;
+
+import java.util.*;
+
 public class RegexEnumerator {
+
+    private final RegexAlphabet alphabet;
+    private final RegexOperationString operationString;
+
+    public RegexEnumerator(RegexAlphabet alphabet, RegexOperationString operationString) {
+        this.alphabet = alphabet;
+        this.operationString = operationString;
+    }
+
+    public List<String> enumerate(int limit) {
+        Set<String> regexes = new HashSet<>();
+        Queue<String> queue = new LinkedList<>();
+
+        for (char c : alphabet.getAlphabet()) {
+            queue.add(String.valueOf(c));
+        }
+
+        while (!queue.isEmpty() && regexes.size() < limit) {
+            String current = queue.poll();
+            if (regexes.add(current)) {
+                for (String regex : new ArrayList<>(regexes)) {
+                    for (char c : alphabet.getAlphabet()) {
+                        String newRegex = operationString.concat(regex, String.valueOf(c));
+                        if (!regexes.contains(newRegex)) {
+                            queue.add(newRegex);
+                        }
+
+                        newRegex = operationString.alternation(regex, String.valueOf(c));
+                        if (!regexes.contains(newRegex)) {
+                            queue.add(newRegex);
+                        }
+
+                        newRegex = operationString.kleene(regex);
+                        if (!regexes.contains(newRegex)) {
+                            queue.add(newRegex);
+                        }
+                    }
+                }
+            }
+        }
+
+        return new ArrayList<>(regexes);
+    }
 
 }
