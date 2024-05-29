c1e952018373c7cda0a6831d60ba9b6c47512a17
Alive <nicolasfr002@gmail.com>
2024-05-29 20:09:48 +0200

    docs(main): added some comments for better understanding
    Took 3 minutes

--- src/de/alive/regexbuilder/Main.java
+++ src/de/alive/regexbuilder/Main.java
@@ -7,16 +7,23 @@ import java.util.List;
 
 public class Main {
     public static void main(String[] args) {
+
+        //Erstelle ein Alphabet mit Empty und epsilon bereits drin
         RegexAlphabet alphabet = new RegexAlphabet(true);
-        alphabet.addCharacter('a', 'b');
+        alphabet.addCharacter('a', 'b'); // füge weitere zeichen zum alphabet hinzu
+
+        //Erstelle ein Objekt, um die RegexStrings zu generieren - mit Anzeige von nur nötigen Klammern
         RegexOperationString operationString = RegexOperationString.create()
                 .setBracketMode(RegexOperationString.BracketMode.NECESSARY)
                 .build();
 
+        //Enumerator Objekt um damit die liste zu erzeugen
         RegexEnumerator enumerator = new RegexEnumerator(alphabet, operationString);
 
+        //erstelle eine liste an regex mit einem limit
         List<String> result = enumerator.enumerate(40);
 
+        //printe die liste
         result.forEach(System.out::println);
     }
 }
