ab737a5a4f99e7bb87c5709a36aba10fe37e236e
Alive <nicolasfr002@gmail.com>
2024-05-29 18:01:07 +0200

    test(main): added testing code
    Took 5 minutes

--- src/de/alive/regexbuilder/Main.java
+++ src/de/alive/regexbuilder/Main.java
@@ -1,7 +1,22 @@
 package de.alive.regexbuilder;
 
+import de.alive.regexbuilder.utils.RegexAlphabet;
+import de.alive.regexbuilder.utils.RegexOperationString;
+
+import java.util.List;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        RegexAlphabet alphabet = new RegexAlphabet(true);
+        alphabet.addCharacter('a', 'b');
+        RegexOperationString operationString = RegexOperationString.create()
+                .setBracketMode(RegexOperationString.BracketMode.NECESSARY)
+                .build();
+
+        RegexEnumerator enumerator = new RegexEnumerator(alphabet, operationString);
+
+        List<String> result = enumerator.enumerate(40);
+
+        result.forEach(System.out::println);
     }
 }
