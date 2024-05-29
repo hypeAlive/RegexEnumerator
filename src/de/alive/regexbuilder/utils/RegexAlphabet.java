c2abf63e9643932a9a21bca82cda8419c7459589
Alive <nicolasfr002@gmail.com>
2024-05-29 19:50:32 +0200

    docs(alphabet): added java doc for alphabet
    Took 5 minutes

--- src/de/alive/regexbuilder/utils/RegexAlphabet.java
+++ src/de/alive/regexbuilder/utils/RegexAlphabet.java
@@ -2,6 +2,10 @@ package de.alive.regexbuilder.utils;
 
 import java.util.*;
 
+/**
+ * Diese Klasse repräsentiert ein Alphabet für reguläre Ausdrücke.
+ * Es kann spezielle Zeichen wie EPSILON und EMPTY enthalten.
+ */
 public class RegexAlphabet {
 
     public static final char EPSILON = '\u03B5';
@@ -10,6 +14,11 @@ public class RegexAlphabet {
     private LinkedHashSet<Character> alphabet;
     private boolean includeSpecialCharacters = false;
 
+    /**
+     * Erstellt ein neues RegexAlphabet.
+     *
+     * @param includeSpecialCharacters Wenn true, werden die speziellen Zeichen EPSILON und EMPTY zum Alphabet hinzugefügt.
+     */
     public RegexAlphabet(boolean includeSpecialCharacters) {
         this.includeSpecialCharacters = includeSpecialCharacters;
         this.alphabet = new LinkedHashSet<>();
@@ -19,12 +28,22 @@ public class RegexAlphabet {
         }
     }
 
+    /**
+     * Fügt die gegebenen Zeichen zum Alphabet hinzu.
+     *
+     * @param c Die hinzuzufügenden Zeichen.
+     */
     public void addCharacter(char... c) {
         for(char ch : c) {
             alphabet.add(ch);
         }
     }
 
+    /**
+     * Entfernt das gegebene Zeichen aus dem Alphabet.
+     *
+     * @param c Das zu entfernende Zeichen.
+     */
     public void removeCharacter(char c) {
         if (c == EPSILON || c == EMPTY) {
             if (!includeSpecialCharacters) {
@@ -35,10 +54,21 @@ public class RegexAlphabet {
         }
     }
 
+    /**
+     * Überprüft, ob das gegebene Zeichen im Alphabet enthalten ist.
+     *
+     * @param c Das zu überprüfende Zeichen.
+     * @return true, wenn das Zeichen im Alphabet enthalten ist, sonst false.
+     */
     public boolean containsCharacter(char c) {
         return alphabet.contains(c);
     }
 
+    /**
+     * Gibt eine Liste der Zeichen im Alphabet zurück.
+     *
+     * @return Eine Liste der Zeichen im Alphabet.
+     */
     public List<Character> getAlphabet() {
         return new ArrayList<>(alphabet);
     }
